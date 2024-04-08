b68964bc1e6a395be292aef618040fe0748e9cf4
BEN AMOR Aziz <benamoraziz0220@gmail.com>
2024-04-09 00:38:48 +0100

    login token

--- frontend/src/app/pages/home-page/home-page.page.ts
+++ frontend/src/app/pages/home-page/home-page.page.ts
@@ -1,18 +1,20 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { EventService } from 'src/app/services/event.service';
-
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.page.html',
   styleUrls: ['./home-page.page.scss'],
+  providers: [UserService]
 })
 export class HomePagePage implements OnInit {
 
   events: any[] = [];
-  constructor(private eventService: EventService, private router:Router) { }
-
+  constructor(private eventService: EventService, private router:Router, public UserService: UserService) { }
+  public userId = this.UserService.getUserDataFromToken();
   ngOnInit(): void {
+    console.log('User ID:', this.userId);
     this.fetchEvents();
   }
 
---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {}
+
+
+
+  getUserDataFromToken(){
+    const token = localStorage.getItem('token')
+    if (!token) {
+      console.log("Token not found")
+    } else {
+      try{
+        let data = JSON.parse(window.atob(token.split('.')[1]))
+        return data
+      } catch (error) {
+        console.error('Error parsing token:', error);
+     }
+    }
+  }
+
+  }
